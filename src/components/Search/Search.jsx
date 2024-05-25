98d8252d50c33b8436d473ff1eb6bb2d8d2d9f9f
MikhailV <mikhail.vinogradov.work@mail.ru>
2024-05-25 11:16:54 +0200

    added debounce for search

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,15 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useCallback, useContext, useState } from 'react';
 
 import styles from './Search.module.scss';
 import { SearchContext } from '../../App';
+import debounce from 'lodash.debounce';
 
 export const Search = () => {
+  const { setInputValue } = useContext(SearchContext);
+  const [searchValue, setSearchValue] = useState('');
 
-  const {inputValue, setInputValue} = useContext(SearchContext)
+  const onChangeSearchDebounce = useCallback(
+    debounce(searchValue => {
+      setInputValue(searchValue);
+    }, 500), []);
+
+  const onChangeSearch = value => {
+    setSearchValue(value);
+    onChangeSearchDebounce(value);
+  };
+
   return (
     <input
-      value={inputValue}
-      onChange={event => setInputValue(event.target.value)}
+      value={searchValue}
+      onChange={event => onChangeSearch(event.target.value)}
       className={styles.root}
       placeholder='Поиск пиццы...'
     />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,45 +6,71 @@ import Sort from '../components/Sort/Sort';
 import { Pagination } from '../components/Pagination/Pagination';
 import { useDispatch, useSelector } from 'react-redux';
 import { setCategoryId } from '../redux/slices/filterSlice';
+import axios from 'axios';
+import qs from 'qs';
+import { useNavigate } from 'react-router-dom';
 
 export const Home = ({ inputValue }) => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
-  // const [sortType, setSortType] = useState('rating');
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const navigate = useNavigate()
+
+  const categoriesActivity = useSelector(state => state.filter.categoryId);
+  const dispatch = useDispatch();
+
+  const setActive = id => {
+    dispatch(setCategoryId(id));
+  };
+
   
-  const [currentPage, setCurrentPage] = useState(1)
 
-  const categoriesActivity = useSelector(state => state.filter.categoryId)
-  const dispatch = useDispatch()
+  const sortType = useSelector(state => state.filter.sortType.sorting);
 
-  const setActive = (id) => {
-    dispatch(setCategoryId(id))
-  }
-
-  const sortType = useSelector(state => state.filter.sortType.sorting)
-
-  let categoriesShowed = categoriesActivity === 0? '' : categoriesActivity
+  let categoriesShowed = categoriesActivity === 0 ? '' : categoriesActivity;
 
   useEffect(() => {
-    setIsLoading(true)
-    fetch('https://6641f7503d66a67b3435a069.mockapi.io/items?page=' + currentPage + '&limit=8&category=' + categoriesShowed + '&sortBy=' + sortType)
-      .then(res => res.json())
+    setIsLoading(true);
+    axios
+      .get(
+        'https://6641f7503d66a67b3435a069.mockapi.io/items?page=' +
+          currentPage +
+          '&limit=8&category=' +
+          categoriesShowed +
+          '&sortBy=' +
+          sortType
+      )
       .then(res => {
-        setItems(res);
+        setItems(res.data);
         setIsLoading(false);
       });
-      window.scrollTo(0, 0)
+    window.scrollTo(0, 0);
   }, [categoriesShowed, sortType, currentPage]);
 
-  let pizzas = items.filter((item) => {
-    return item.title.toLowerCase().includes(inputValue)
-  }).map(obj => <PizzaBlock key={obj.id} {...obj} />)
+  useEffect(() => {
+    const queryString = qs.stringify({
+      sortType,
+      categoriesShowed,
+      currentPage,
+    })
+    navigate(`?${queryString}`)
+  }, [categoriesShowed, sortType, currentPage, navigate])
+
+  let pizzas = items
+    .filter(item => {
+      return item.title.toLowerCase().includes(inputValue);
+    })
+    .map(obj => <PizzaBlock key={obj.id} {...obj} />);
 
   return (
     <div className='container'>
       <div className='content__top'>
-        <Categories activity={categoriesActivity} setActive={(activityId) => setActive(activityId)} />
+        <Categories
+          activity={categoriesActivity}
+          setActive={activityId => setActive(activityId)}
+        />
         <Sort />
       </div>
       <h2 className='content__title'>Все пиццы</h2>
@@ -53,7 +79,11 @@ export const Home = ({ inputValue }) => {
           ? [...new Array(8)].map((_, index) => <Skeleton key={index} />)
           : pizzas}
       </div>
-      {categoriesActivity === 0 ? <Pagination onChangePage={number => setCurrentPage(number)}/> : ''}
+      {categoriesActivity === 0 ? (
+        <Pagination onChangePage={number => setCurrentPage(number)} />
+      ) : (
+        ''
+      )}
     </div>
   );
 };
